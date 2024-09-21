be7dd832e9656f739c532a56f22c4e332a79ee6b
Andue <14904096+andue@user.noreply.gitee.com>
2024-09-21 18:32:24 +0800

    cs-easy-02

--- cs-easy-02-1.c
+++ cs-easy-02-1.c
@@ -1,134 +1,166 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Node {
-    int data;  
-    struct Node* next;  
+// 格式化文档了，真的是自己写的qwq
+struct Node
+{
+    int data;
+    struct Node *next;
 };
 
-struct Node* h = NULL;  
+struct Node *h = NULL;
 
-struct Node* createNode(int x) {
-    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
-    p->data = x;  
-    p->next = NULL;  
+struct Node *createNode(int x)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    p->data = x;
+    p->next = NULL;
     return p;
 }
 
-void init() {
+void init()
+{
     h = createNode(1);
-    h->next = h;  
+    h->next = h;
 }
 
-void H(int x) {
-    struct Node* p = createNode(x);
-    if (h == NULL) {
+void H(int x)
+{
+    struct Node *p = createNode(x);
+    if (h == NULL)
+    {
         h = p;
-        p->next = h; 
+        p->next = h;
         return;
     }
-    struct Node* t = h;
-    while (t->next != h) {
+    struct Node *t = h;
+    while (t->next != h)
+    {
         t = t->next;
     }
     p->next = h;
     t->next = p;
-    h = p; 
+    h = p;
 }
 
-void T(int x) {
-    struct Node* p = createNode(x);
-    if (h == NULL) {
+void T(int x)
+{
+    struct Node *p = createNode(x);
+    if (h == NULL)
+    {
         h = p;
         h->next = h;
         return;
     }
-    struct Node* t = h;
-    while (t->next != h) {
+    struct Node *t = h;
+    while (t->next != h)
+    {
         t = t->next;
     }
     t->next = p;
-    p->next = h; 
+    p->next = h;
 }
 
-void D(int l) {
-    if (h == NULL) return;
+void D(int l)
+{
+    if (h == NULL)
+        return;
 
-    struct Node* t = h;
-    if (l == 1) {
-        if (h->next == h) { 
+    struct Node *t = h;
+    if (l == 1)
+    {
+        if (h->next == h)
+        {
             free(h);
             h = NULL;
             return;
         }
-        struct Node* rm = h;
-        while (t->next != h) {
+        struct Node *rm = h;
+        while (t->next != h)
+        {
             t = t->next;
         }
-        h = h->next; 
-        t->next = h; 
+        h = h->next;
+        t->next = h;
         free(rm);
         return;
     }
-    for (int i = 1; i < l - 1; i++) {
+    for (int i = 1; i < l - 1; i++)
+    {
         t = t->next;
-        if (t->next == h) return; 
+        if (t->next == h)
+            return;
     }
-    if (t->next == h) return; 
-    struct Node* rm = t->next;
+    if (t->next == h)
+        return;
+    struct Node *rm = t->next;
     t->next = rm->next;
     free(rm);
 }
-void C() {
-    if (h == NULL) return; 
-    struct Node* end = h; 
-    while (end->next != h) {
+void C()
+{
+    if (h == NULL)
+        return;
+    struct Node *end = h;
+    while (end->next != h)
+    {
         end = end->next;
     }
     end->next = h;
 }
-void print(FILE* f) {
-    if (h == NULL) {
+void print(FILE *f)
+{
+    if (h == NULL)
+    {
         fprintf(f, "NULL\n");
         return;
     }
-    struct Node* t = h;
-    do {
-        fprintf(f, "%d -> ", t->data); 
-        t = t->next; 
+    struct Node *t = h;
+    do
+    {
+        fprintf(f, "%d -> ", t->data);
+        t = t->next;
     } while (t != h);
     fprintf(f, "NULL\n");
 }
 
-int main() {
+int main()
+{
     init();
-    
-    FILE *input = freopen("input.in", "r",stdin);
-    FILE *output = freopen("output.out", "w",stdout);
+
+    FILE *input = freopen("input.in", "r", stdin);
+    FILE *output = freopen("output.out", "w", stdout);
 
     char x;
     int a, b, c, l;
 
-    while (fscanf(input, " %c", &x) != EOF) {
-        if (x == 'H') {
+    while (fscanf(input, " %c", &x) != EOF)
+    {
+        if (x == 'H')
+        {
             fscanf(input, "%d %d %d", &a, &b, &c);
             H(c);
             H(b);
             H(a);
-        } else if (x == 'T') {
+        }
+        else if (x == 'T')
+        {
             fscanf(input, "%d %d %d", &a, &b, &c);
             T(a);
             T(b);
             T(c);
-        } else if (x == 'D') {
+        }
+        else if (x == 'D')
+        {
             fscanf(input, "%d", &l);
             D(l);
-        } else C();
+        }
+        else
+            C();
         print(output);
     }
 
     fclose(stdin);
     fclose(stdout);
-    
+
     return 0;
 }
--- cs-easy-02-2.c
+++ cs-easy-02-2.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
-struct Node* createNode(int d) {
-    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+struct Node *createNode(int d)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
     n->data = d;
     n->next = n;
     return n;
 }
 
-void insertNode(struct Node** h, int d) {
-    struct Node* n = createNode(d);
-    if (*h == NULL) {
+void insertNode(struct Node **h, int d)
+{
+    struct Node *n = createNode(d);
+    if (*h == NULL)
+    {
         *h = n;
-    } else {
-        struct Node* t = *h;
-        while (t->next != *h) {
+    }
+    else
+    {
+        struct Node *t = *h;
+        while (t->next != *h)
+        {
             t = t->next;
         }
         t->next = n;
@@ -27,43 +34,50 @@ void insertNode(struct Node** h, int d) {
     n->next = *h;
 }
 
-void josephus(struct Node** h, int m, FILE* output) {
-    if (*h == NULL) return;
+void josephus(struct Node **h, int m, FILE *output)
+{
+    if (*h == NULL)
+        return;
 
-    struct Node* c = *h;
-    struct Node* p = NULL;
+    struct Node *c = *h;
+    struct Node *p = NULL;
 
-    while (c->data != 3) {
+    while (c->data != 3)
+    {
         c = c->next;
     }
 
-    while (c->next != c) {
-        for (int i = 1; i < m; i++) {
+    while (c->next != c)
+    {
+        for (int i = 1; i < m; i++)
+        {
             p = c;
             c = c->next;
         }
         fprintf(output, "%d", c->data);
         p->next = c->next;
-        struct Node* t = c;
+        struct Node *t = c;
         c = c->next;
         free(t);
     }
-    fprintf(output, "%d", c->data);
+    fprintf(output, "%d\n", c->data);
     free(c);
 }
 
-int main() {
-    struct Node* h = NULL;
+int main()
+{
+    struct Node *h = NULL;
 
-    int values[] = {1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1, 1, 3, 1, 1, 2, 1, 2, 2, 2, 2, 1, 1, 2, 2, 1, 1, 1};
-    int size = sizeof(values) / sizeof(values[0]);
+    int num[] = {1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1, 1, 3, 1, 1, 2, 1, 2, 2, 2, 2, 1, 1, 2, 2, 1, 1, 1};
+    int size = sizeof(num) / sizeof(*num);
 
-    for (int i = 0; i < size; i++) {
-        insertNode(&h, values[i]);
+    for (int i = 0; i < size; i++)
+    {
+        insertNode(&h, num[i]);
     }
 
-    FILE* output = freopen("Josephus.out", "w", stdout);
-    
+    FILE *output = freopen("Josephus.out", "w", stdout);
+
     int m = 3;
     josephus(&h, m, output);
 
